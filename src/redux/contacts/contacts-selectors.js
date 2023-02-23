a05d7a7ee198d068b3ddbee9c66139eb29c8b826
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-02-23 21:55:24 +0200

    first

---
+++ src/redux/contacts/contacts-selectors.js
@@ -0,0 +1,19 @@
+export const getAllContacts = ({contacts}) => contacts;
+export const filter = ({filter}) => filter;
+
+
+export const getFilteredContacts = ({contacts, filter}) => {
+    if (!filter) {
+        return contacts;
+    }
+    const getVisibleContacts = () => {
+    const lowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(lowerCaseFilter),
+    );
+    };
+    
+    const visibleContacts = getVisibleContacts();
+
+    return visibleContacts;
+}
