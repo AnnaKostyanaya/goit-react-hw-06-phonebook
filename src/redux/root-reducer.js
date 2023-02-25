ad33ad2225b07f83212016b422c1f08edd6b0883
Hanna Kostiana <anna.kostianaia@gmail.com>
2023-02-25 19:33:01 +0200

    change reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,17 +5,31 @@ import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from "./contacts/contacts-slice";
 import { filterReducer } from "./filter/filter-slice";
 
-const rootReducer = combineReducers({
-    contacts: contactsReducer,
-    filter: filterReducer,
-})
+// const rootReducer = combineReducers({
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+// })
+
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['contacts']
+        
+// }
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// export default persistedReducer;
 
 const persistConfig = {
     key: 'root',
     storage,
-    
+    whitelist: ['contacts']      
 }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const rootReducer = combineReducers({
+    contacts: persistReducer(persistConfig, contactsReducer),
+    filter: filterReducer,
+})
 
-export default persistedReducer;
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,11 +9,11 @@ import {
     REGISTER,
 } from 'redux-persist';
 
-// import rootReducer from "./root-reducer";
-import persistedReducer from "./root-reducer";
+import rootReducer from "./root-reducer";
+// import persistedReducer from "./root-reducer";
 
 export const store = configureStore({
-    reducer: persistedReducer,
+    reducer: rootReducer,
     middleware(getDefaultMiddleware) {
         return getDefaultMiddleware({
             serializableCheck: {
